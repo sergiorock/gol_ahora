3af8809bf933f42ef5408e14e5046ecca5dbb165
sergiorock <lopez.sergio.n@gmail.com>
2026-05-06 12:03:08 -0300

    Corrige before_action set_user para incluir accion pdf

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::BaseController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[show edit update destroy pdf]
 
   def index
     @users = User.order(:last_name, :first_name)
