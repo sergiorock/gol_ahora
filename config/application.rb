f68bef95cb481a07bc19d59d25843dd5c9bdf522
sergiorock <lopez.sergio.n@gmail.com>
2026-05-06 02:36:45 -0300

    Agrega locale es-AR para Devise y configura zona horaria Buenos Aires

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,8 @@ module GolAhora
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :"es-AR"
+    config.i18n.available_locales = [:"es-AR"]
+    config.time_zone = "Buenos Aires"
   end
 end
