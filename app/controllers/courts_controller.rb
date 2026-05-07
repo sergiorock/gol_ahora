5315c5496427e81ddf6175bb3f343732c04ed00e
sergiorock <lopez.sergio.n@gmail.com>
2026-05-07 13:31:59 -0300

    Mejora filtros de canchas: tamaño y superficie combinables de forma independiente

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -2,9 +2,13 @@ class CourtsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @court_types = CourtType.order(:name)
+    @sizes    = CourtType.distinct.order(:name).pluck(:name)
+    @surfaces = CourtType.distinct.order(:surface).pluck(:surface).compact
+
     @courts = Court.includes(:court_type).available.order(:name)
-    @courts = @courts.where(court_type_id: params[:type]) if params[:type].present?
+    @courts = @courts.joins(:court_type).where(court_types: { name: params[:size] })       if params[:size].present?
+    @courts = @courts.joins(:court_type).where(court_types: { surface: params[:surface] }) if params[:surface].present?
+
     authorize @courts
   end
 
