27486465f090ec7c86cb90a2ccc328b594ffbac9
sergiorock <lopez.sergio.n@gmail.com>
2026-06-05 17:19:17 -0300

    Redesign home landing experience

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @stats = [
+      [ Reservation.where.not(status: "cancelled").count, "reservas registradas" ],
+      [ User.client.count, "jugadores registrados" ],
+      [ Court.available.count, "canchas activas" ],
+      [ "24/7", "reserva web" ]
+    ]
   end
 end
